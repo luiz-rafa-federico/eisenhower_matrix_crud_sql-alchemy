c33d6155c9a3a42ab6faf88dd6a02d7aa287f7e7
luizrafa-federico <luizrafa.federico@gmail.com>
2021-12-05 10:44:44 -0300

    get_all( )

--- app/controllers/categories_controller.py
+++ app/controllers/categories_controller.py
@@ -1,7 +1,10 @@
 from flask import jsonify, request, current_app
 from app.models.categories_model import Categories
 from app.models.eisenhowers_model import Eisenhowers
+from app.models.tasks_model import Tasks
+from app.models.tasks_categories_table import tasks_categories
 from sqlalchemy import exc
+from app.configs.database import db
 
 
 def create_category(categ=None):
@@ -47,4 +50,17 @@ def delete_category(id: int):
 
 
 def get_all():
-    ...
+    query = db.session.query(Categories, Tasks, Eisenhowers).select_from(Categories).join(tasks_categories).join(Tasks).join(Eisenhowers).all()
+
+    for category, task, eisenhower in query:
+        return jsonify([{
+            "id": category.id,
+            "name": category.name,
+            "description": category.description,
+            "tasks": [{
+                "id": task.id,
+                "name": task.name,
+                "description": task.description,
+                "priority": eisenhower.type
+            } for category.id, task.id, eisenhower.type in query] 
+        }])
